28ba59d0d80b15f84bd68166fd08f22974849e3f
Guilherme Alves <guilherme.amaro@ibm.com>
2021-04-03 11:22:41 -0300

    removes ign news call due instability

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from omelete_scraper import OmeleteScraper
-from ign_scraper import IGNScraper
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
@@ -9,12 +8,10 @@ cors = CORS(app, resources={r"*": {"origins": "*"}})
 @app.route('/news')
 def get_news():
   omelete = OmeleteScraper()
-  ign = IGNScraper()
 
   omelete_news = omelete.get_omelete_news()
-  ign_news = ign.get_ign_news()
 
-  news = omelete_news + ign_news
+  news = omelete_news
 
   return jsonify({'news': news}), 200
 
