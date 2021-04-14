55424371143dab7340306143a14dcb39b3f74005
Guilherme Alves <guilherme.amaro@ibm.com>
2021-04-14 19:25:03 -0300

    cache news response

--- app.py
+++ app.py
@@ -3,12 +3,16 @@ from flask import Flask, jsonify, request
 from flask_cors import CORS
 from omelete_scraper import OmeleteScraper
 from gamespot_scraper import GamespotScraper
+from cache import cache
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"*": {"origins": "*"}})
+cache.init_app(app)
 
 @app.route('/news')
+@cache.cached(timeout=21600, key_prefix='news')
 def get_news():
+
   omelete = OmeleteScraper()
   gamespot = GamespotScraper()
 
